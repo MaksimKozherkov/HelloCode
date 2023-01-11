6b0695c4cfa7d86af24e84443e616b149e78526b
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-11 17:49:12 +0300

    Добавили задачу на замену в тексте

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -87,11 +87,49 @@ string res = Method4(10, "asdf");
 //Console.WriteLine(res);
 
 //Вид 4 Цикл в цикле(таблица умножения)
-for (int i = 2; i <= 10; i++)
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+//}
+
+
+//Работа с текстом
+//Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винцегероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+//string s = "qwerty"
+//            012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)//Replace это то как мы назвали метод
 {
-    for (int j = 2; j <= 10; j++)
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-        Console.WriteLine($"{i} x {j} = {i * j}");
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    Console.WriteLine();
+
+    return result;
 }
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
