5b3940e09923abdd4a978bef964407afc4ce67dc
Maksim Kozherkov <Mirex5915@gmail.com>
2023-01-11 18:14:33 +0300

    Добавили задачу на упорядочивание чисел

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -74,7 +74,7 @@ int year = Method3();
 // }
 
 string Method4(int count, string text)//Метод который что то принимает и что то возвращает
-{ 
+{
     string result = String.Empty;
     for(int i = 0; i < count; i++)//for вместо while, for компанует все условия цикла
     {
@@ -128,8 +128,45 @@ string Replace(string text, char oldValue, char newValue)//Replace это то �
     return result;
 }
 
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
